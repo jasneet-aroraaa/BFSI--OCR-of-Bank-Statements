546dac96f7fc07f4f1ab7c60f072c5f1ed04dc65
jasneet-aroraaa <jasneet.arora272004@gmail.com>
2025-01-02 20:12:27 +0530

    Update main.py

--- Milestone4/main.py
+++ Milestone4/main.py
@@ -30,9 +30,9 @@ if 'cloudinary_images' not in st.session_state:
 # Load environment variables and configure Cloudinary
 load_dotenv()
 cloudinary.config(
-    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+    cloud_name=st.secrets["cloudinary"]["CLOUDINARY_CLOUD_NAME"],
+    api_key=st.secrets["cloudinary"]["CLOUDINARY_API_KEY"],
+    api_secret=st.secrets["cloudinary"]["CLOUDINARY_API_SECRET"]
 )
 
 def create_requests_session(retries=3, backoff_factor=0.3, status_forcelist=(500, 502, 504)):
